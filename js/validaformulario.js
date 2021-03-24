69e3930c8240f5284d439e8cc45e339e9e013cc5
Gabriel <usilvar1700@alumno.ipn.mx>
2021-03-23 20:17:36 -0600

    formilario

--- js/validaformulario.js
+++ js/validaformulario.js
@@ -11,6 +11,72 @@ function validar(formulario){
         formulario.nombre.focus();
         return false;
     }
-}
 
+
+
+    var checkOK="abcdefghijklmnñopqrstuvwxyz" + "ABCDEFGHIJKLMNñOPQRSTUVWXYZ";
+
+    var checkStr = formulario.nombre.value;
+
+    var allvalid = true;
+
+    for(var i=0; i < checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+        for (var j = 0; j < checkOK.length; j++)
+        if (ch == checkOK.charAt(j))
+            break;
+
+
+        if (j == checkOK.length){
+            allvalid = false;
+            break;
+        }
+    }
+        
+
+    if(!allvalid){
+        alert("Escriba solo letras en el campo nombre")
+        formulario.nombre.focus();
+        return false;
+    }
+
+    
+
+    var checkOK="123456789";
+
+    var checkStr = formulario.edad.value;
+
+    var allvalid = true;
+
+    for(var i=0; i < checkStr.length; i++){
+        var ch = checkStr.charAt(i);
+        for (var j = 0; j < checkOK.length; j++)
+        if (ch == checkOK.charAt(j))
+            break;
+
+
+        if (j == checkOK.length){
+            allvalid = false;
+            break;
+        }
+    }
+        
+
+    if(!allvalid){
+        alert("Escriba numeros en el campo edad")
+        formulario.edad.focus();
+        return false;
+    }
+
+
+    var txt = formulario.email.value;
+    var b = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/;
+
+    alert("Email "+(b.test(txt)?"":"no")+" valido");
+
+    return b.test(txt);
+
+
+
+}
 
